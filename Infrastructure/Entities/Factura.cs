a532b143841507cb615d76b777cb85d1b28df45f
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2025-02-20 10:53:44 -0500

    change data type for the fecha factura property

--- Infrastructure/Entities/Factura.cs
+++ Infrastructure/Entities/Factura.cs
@@ -4,7 +4,7 @@ public class Factura
 {
     public int Id { get; set; }
     public string Cliente { get; set; } = string.Empty;
-    public DateTime Fecha { get; set; }
+    public DateOnly Fecha { get; set; }
     public decimal Total { get; set; }
 
     public virtual List<DetalleFactura> Detalles { get; set; } = new();
