9c721b5032c6991e46525d2aa810d93110309a0c
Jan Carlos Anaya Jiménez <janaya@unimagdalena.edu.co>
2025-02-20 00:15:05 -0500

    configure the app db context

---
+++ Infrastructure/Extensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Com.TaxiMarino.Services.AdministradorFacturas.API.Infrastructure;
+
+public static class Extensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<DbContext, AppDbContext>();
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("AppDbContext") ?? throw new ArgumentNullException("AppDbContext"))
+                .LogTo(Console.WriteLine, LogLevel.Information);
+            options.UseLazyLoadingProxies();
+        });
+    }
+}
